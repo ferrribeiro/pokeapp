57fd8849ece9949f4c922b6b846502c2a96f26d2
Raissa Correa <raissacorreaa@gmail.com>
2021-01-23 10:28:02 -0300

    Atualização do Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return <div className="header">Pokedex</div>;
+}
+//colocar meus pkm e todos pkm no cabeçalho e filtro
+export default Header;
